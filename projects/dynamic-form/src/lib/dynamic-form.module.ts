c14f0e97c5c0d1a918dab6f9a7c73554fea54eea
Chami <gilbert.chami@ega.go.tz>
2021-02-17 09:08:13 +0300

    some working changes

--- projects/dynamic-form/src/lib/dynamic-form.module.ts
+++ projects/dynamic-form/src/lib/dynamic-form.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatOptionModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { AttachmentComponent } from './attachment/attachment.component';
 import { ButtonComponent } from './button/button.component';
@@ -45,7 +47,8 @@ import { TextareaComponent } from './textarea/textarea.component';
     FormsModule,
     CommonModule,
     ReactiveFormsModule,
-
+    MatFormFieldModule,
+    MatInputModule,
     MatSelectModule,
     MatOptionModule,
     MatIconModule
